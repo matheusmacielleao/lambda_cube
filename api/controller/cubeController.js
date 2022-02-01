40672222ab4d344454978ed6c6ccd708c0df97cb
matheusmacielleao <matheusmacielleao@gmail.com>
2022-02-01 11:20:01 -0300

    feat(cube): calc Volume funcion

--- api/controller/cubeController.js
+++ api/controller/cubeController.js
@@ -1,6 +1,15 @@
-'use strict';
 
 module.exports.calcVolume = async (event) => {
-    response = {statusCode: 204}
+    let response = {statusCode: 204}
+    event = JSON.parse(event.body)
+    const cube = {
+        width : event.width,
+        height : event.height,
+        depth : event.depth,
+        volume :event.width * event.height * event.depth
+    }
+    
+    response.body = JSON.stringify(cube)
+    console.log(response);
     return response;
 };

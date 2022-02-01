8e73691739e3f478efbd94131b991bed775cc3f6
matheusmacielleao <matheusmacielleao@gmail.com>
2022-02-01 13:46:07 -0300

    feat:validation  for request.body

--- api/controller/cubeController.js
+++ api/controller/cubeController.js
@@ -1,12 +1,21 @@
 module.exports.calcVolume = async (event) => {
   const response = { statusCode: 201 };
   const body = JSON.parse(event.body);
+
   const cube = {
-    width: body.width,
-    height: body.height,
-    depth: body.depth,
-    volume: body.width * body.height * body.depth,
+    width: Number(body.width),
+    height: Number(body.height),
+    depth: Number(body.depth),
+    volume: Number(body.width * body.height * body.depth),
   };
+
+  Object.keys(cube).forEach((value) => {
+    if (!cube[value]) {
+      throw new Error(`missing atribute ${value}`);
+    } else if (!(cube[value] > 0 && typeof cube[value] === 'number')) {
+      throw new Error(`invalid value for atribute ${value}`);
+    }
+  });
 
   response.body = JSON.stringify(cube);
   return response;

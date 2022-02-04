5cfa343a4304eac9dc3a7446fcaeb2813fb200c1
matheusmacielleao <matheusmacielleao@gmail.com>
2022-02-04 16:09:49 -0300

    tests: test for chached cube

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,14 @@
+const { createClient } = require('redis');
 const { calcVolume } = require('../src');
 
 describe('cubeValidation', () => {
   let cubeMock = null;
+  afterAll(async () => {
+    const client = createClient();
+    await client.connect();
+    await client.del('cubes');
+    await client.disconnect();
+  });
 
   beforeAll(async () => {
     cubeMock = {
@@ -9,12 +16,25 @@ describe('cubeValidation', () => {
       depth: 10,
       width: 10,
     };
+    const client = createClient();
+    await client.connect();
+    await client.del('cubes');
+    await client.disconnect();
   });
   test('should calculate the volume of the cube', async () => {
     const response = await calcVolume({ body: JSON.stringify(cubeMock) });
     response.body = JSON.parse(response.body);
     expect(response.statusCode).toBe(201);
     expect(response.body.volume).toBe(1000);
+  });
+  test('should return cached cube', async () => {
+    let response = await calcVolume({ body: JSON.stringify(cubeMock) });
+    response = await calcVolume({ body: JSON.stringify(cubeMock) });
+    response.body = JSON.parse(response.body);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.volume).toBe(1000);
+    expect(response.body.cached).toBe(true);
+
   });
   test('should return error with invalid cube', async () => {
     delete cubeMock.width;

9b0beac496f90e14a7cfe41aeea0f85ded6c4ae0
matheusmacielleao <matheusmacielleao@gmail.com>
2022-02-04 15:49:58 -0300

    feat: saving cubes to cache

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,32 @@
+import { readJsonConfigFile } from 'typescript';
 import {cubeValidation} from './validation/cube/cube-validation';
+import { createClient } from 'redis';
+
 
 const calcVolume = async (event:{body: string}) => {
   try {
     let body = JSON.parse(event.body);
     if (body === null) { body = {}; }
+   
+    const client = createClient();
+    await client.connect();
 
-      cubeValidation(body);
-
-    const cube = {
+    cubeValidation(body);
+    const stringifiedBody = JSON.stringify(body);
+    const cached = await client.HGET("cubes",stringifiedBody);
+    if(cached){
+      return {body: cached,statusCode:200}
+    }
+    const cube = JSON.stringify({
       width: body.width,
       height: body.height,
       depth: body.depth,
       volume: body.width * body.height * body.depth,
-    };
-    const response = { body: JSON.stringify(cube), statusCode: 201 };
+      cached: true,
+    });
+
+    const response = { body: cube, statusCode: 201 };
+    client.HSET("cubes",stringifiedBody,cube);
     return response;
   } catch (e : any) {
     return { body: e.message, statusCode: 400 };

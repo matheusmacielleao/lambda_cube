49bd4471b44f1b2e4901ce1a25d2041c56f3ff10
matheusmacielleao <matheusmacielleao@gmail.com>
2022-02-07 10:07:22 -0300

    fix: removed unescessary awaits

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { readJsonConfigFile } from 'typescript';
 import {cubeValidation} from './validation/cube/cube-validation';
 import { createClient } from 'redis';
 
@@ -6,34 +5,36 @@ import { createClient } from 'redis';
 const calcVolume = async (event:{body: string}) => {
   const client = createClient();
   await client.connect();
+  
   try {
     let body = JSON.parse(event.body);
     if (body === null) { body = {}; }
    
-    
     cubeValidation(body);
     
-    const stringifiedBody = JSON.stringify(body);
+    const stringifiedBody = event.body;
     const cached = await client.HGET("cubes",stringifiedBody);
     if(cached){
-      await client.quit();
-      return {body: cached,statusCode:200}
+      client.quit();
+      const cachedCube = JSON.parse(cached);
+      cachedCube.cached = true;
+      return {body: JSON.stringify(cachedCube),statusCode:200}
     }
     const cube = JSON.stringify({
       width: body.width,
       height: body.height,
       depth: body.depth,
       volume: body.width * body.height * body.depth,
-      cached: true,
     });
 
     const response = { body: cube, statusCode: 201 };
-    await client.HSET("cubes",stringifiedBody,cube);
-    await client.quit();
+    
+    client.HSET("cubes",stringifiedBody,cube).then(()=>client.quit());
+        
     return response;
 
   } catch (e : any) {
-    await client.quit();
+    client.quit();
     return { body: e.message, statusCode: 400 };
   }
 };
